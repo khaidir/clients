9d24d7fbc045304c26bdf9b05276b49555aed8eb
GUW STUDIO <67969716+guwstudio@users.noreply.github.com>
2025-01-24 18:35:56 +0700

    up-content

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -51,24 +51,27 @@
                             <form action="{{ url('login') }}"" class="form w-100" method="POST">
                                 @csrf
                                 <div class="text-center mb-11">
-                                    <h1 class="text-gray-900 fw-bolder mb-3">
+                                <a href="/" class="mb-0 mb-lg-12">
+                                <img alt="Logo" src="/images/clients-2.png" class="h-60px h-lg-75px"/>
+                                </a>
+                                    <h1 class="text-gray-900 fw-bolder mb-3 mt-7">
                                         Sign In
                                     </h1>
                                     <div class="text-gray-500 fw-semibold fs-6">
                                         Client Segmention
                                     </div>
                                 </div>
-                                <div class="row g-3 mb-9">
+                                <!-- <div class="row g-3 mb-9">
                                     <div class="col-md-12">
                                         <a href="#" class="btn btn-flex btn-outline btn-text-gray-700 btn-active-color-primary bg-state-light flex-center text-nowrap w-100">
                                         <img alt="Logo" src="/assets/media/svg/brand-logos/google-icon.svg" class="h-15px me-3"/>
                                         Sign in with Google
                                         </a>
                                     </div>
-                                </div>
-                                <div class="separator separator-content my-14">
+                                </div> -->
+                                <!-- <div class="separator separator-content my-14">
                                     <span class="w-125px text-gray-500 fw-semibold fs-7">Or with email</span>
-                                </div>
+                                </div> -->
                                 <div class="fv-row mb-8">
                                     <input type="text" placeholder="Email" name="email" autocomplete="off" value="{{ old('email') }}" class="form-control bg-transparent @error('email') is-invalid @enderror"/>
                                     @if ($errors->has('email'))
@@ -81,14 +84,14 @@
                                         <span class="text-danger">{{ $errors->first('password') }}</span>
                                     @endif
                                 </div>
-                                <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
+                                <!-- <div class="d-flex flex-stack flex-wrap gap-3 fs-base fw-semibold mb-8">
                                     <div></div>
                                     <a href="/auth/forgot" class="link-primary">
                                     Forgot Password ?
                                     </a>
-                                </div>
+                                </div> -->
                                 <div class="d-grid mb-10">
-                                    <button type="submit" id="" class="btn btn-primary">
+                                    <button type="submit" id="" class="btn btn-warning">
                                         <span class="indicator-label">
                                         Sign In</span>
                                         <span class="indicator-progress">
@@ -100,20 +103,14 @@
                         </div>
                     </div>
                 </div>
-                <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url(/assets/media/misc/auth-bg.png)">
+                <div class="d-flex flex-lg-row-fluid w-lg-50 bgi-size-cover bgi-position-center order-1 order-lg-2" style="background-image: url(/assets/media/misc/bg_login.png);   border-top-left-radius:20%">
                     <div class="d-flex flex-column flex-center py-7 py-lg-15 px-5 px-md-15 w-100">
-                        <a href="/" class="mb-0 mb-lg-12">
-                        <img alt="Logo" src="/images/clients-white-2.png" class="h-60px h-lg-75px"/>
-                        </a>
-                        <img class="d-none d-lg-block mx-auto w-275px w-md-50 w-xl-500px mb-10 mb-lg-20" src="/images/vektor_pabrik.png" alt=""/>
-                        <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-7">
+                        <img class="d-none d-lg-block mx-auto w-375px w-md-50 w-xl-500px mb-10 mb-lg-20" src="/images/img_login.jpg" style="border-radius: 5%;" alt=""/>
+                        <h1 class="d-none d-lg-block text-white fs-2qx fw-bolder text-center mb-5">
                             Solution of Approval
                         </h1>
                         <div class="d-none d-lg-block text-white fs-base text-center">
-                            In this kind of post, <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the blogger</a>
-                            introduces a person they’ve interviewed <br/> and provides some background information about
-                            <a href="#" class="opacity-75-hover text-warning fw-bold me-1">the interviewee</a>
-                            and their <br/> work following this is a transcript of the interview.
+                        The Approval Application is a software solution designed to streamline and manage approval workflows within an organization. 
                         </div>
                     </div>
                 </div>
